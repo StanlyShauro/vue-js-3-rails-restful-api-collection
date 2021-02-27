e99632e48a716564e45d0ecf207c7f8a2fe6ddd6
Stanley <de10.shauro@gmail.com>
2021-02-27 19:02:51 +0700

    add App.vue example

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <editable-list
+    :list_url='verbs_url'
+    :item_name='item_name'
+    :fields='verb_fields'
+  ></editable-list>
+</template>
+
+<script>
+  import { defineComponent } from "vue";
+  import EditableList from './EditableList';
+  
+export default defineComponent({
+  name: "App",
+  components: {
+    EditableList,
+  },
+  setup() {
+    const verbs_url = "/posts"
+    const item_name = 'post'
+
+    const verb_fields = [
+      { input_type: 'string', input_field: 'name' },
+      { input_type: 'string', input_field: 'content' },
+    ]
+
+  return {
+      verbs_url,
+      verb_fields,
+      item_name
+    };
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
